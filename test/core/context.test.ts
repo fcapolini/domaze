672c4bb1196e5605e297279893811c4b475fe15c
fcapolini <fabrizio.capolini@me.com>
2024-12-19 19:22:32 +0100

    context

--- test/core/context.test.ts
+++ test/core/context.test.ts
@@ -9,3 +9,21 @@ it('should create a context', () => {
   expect(ctx.root.__parent).toBe(ctx.global);
   expect(ctx.cycle).toBe(1);
 });
+
+it('should write-protect the global object', () => {
+  const ctx = new Context({ root: { v1: { e: function() { return 1; } } } });
+  let globalErr: any;
+  let rootErr: any;
+  try {
+    ctx.global['console'] = null;
+  } catch (err) {
+    globalErr = err;
+  }
+  try {
+    ctx.root['v1'] = null;
+  } catch (err) {
+    rootErr = err;
+  }
+  expect(globalErr).toBeDefined();
+  expect(rootErr).toBeUndefined();
+})
