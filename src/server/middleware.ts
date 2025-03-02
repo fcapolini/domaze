827ac6aba4242230cc630c191ae275e9b491e64f
fcapolini <fabrizio.capolini@me.com>
2025-03-02 18:20:25 +0100

    server

--- src/server/middleware.ts
+++ src/server/middleware.ts
@@ -1,2 +1,109 @@
+import { DomazeLogger } from "./logger";
+import { NextFunction, Request, Response } from 'express';
+import path from 'path';
+import fs from 'fs';
+import { Compiler } from "../compiler/compiler";
+import { Document, Element, NodeType } from "../html/dom";
+import { PageError } from "../html/parser";
 
-//TODO
+export const CLIENT_CODE_REQ = '/.domaze.js';
+
+export interface DomazeProps {
+  docroot: string;
+  ssr?: boolean;
+  csr?: boolean;
+  logger?: DomazeLogger;
+  clientCodePath?: string;
+  // virtualFiles?: Array<VirtualFile>;
+}
+
+export function domaze(props: DomazeProps) {
+  props.clientCodePath ??= path.join(__dirname, 'client.js');
+  const docroot = props.docroot || process.cwd();
+  const compiler = new Compiler({ docroot });
+  const clientCode = props.csr
+    ? fs.readFileSync(props.clientCodePath).toString()
+    : '';
+
+  return async function(req: Request, res: Response, next: NextFunction) {
+    const i = req.path.lastIndexOf('.');
+    const extname = i < 0 ? '.html' : req.path.substring(i).toLowerCase();
+
+    // handle non-page requests
+    if (req.path === CLIENT_CODE_REQ) {
+      res.header('Content-Type', 'text/javascript;charset=UTF-8');
+      res.send(clientCode);
+      return;
+    }
+    if (req.path.startsWith('/.') || extname === '.htm') {
+      res.sendStatus(404);
+      return;
+    }
+    if (extname !== '.html') {
+      return next();
+    }
+
+    // if path is a dir, access <dir>/index.html
+    let pathname = i < 0 ? req.path : req.path.substring(0, i).toLowerCase();
+    if (i < 0) {
+      try {
+        const fullPath = path.join(docroot, pathname);
+        const stat = await fs.promises.stat(fullPath);
+        if (stat.isDirectory()) {
+          pathname = path.join(pathname, 'index');
+        }
+      } catch (ignored) { /* nop */ }
+    }
+
+    const page = await compiler.compile(pathname + '.html');
+    if (page.source.errors.length) {
+      return serveErrorPage(page.source.errors, res);
+    }
+
+    let doc: Document = page.source.doc!;
+
+    if (props.ssr) {
+      // doc = (doc as ServerDocument).clone(null, null);
+      // new RuntimePage(
+      //   page => new ServerGlobal(page, doc, comp.props!)
+      // );
+    }
+
+    if (props.csr && doc && doc.documentElement) {
+      for (const n of doc.documentElement.childNodes) {
+        if (
+          n.nodeType === NodeType.ELEMENT &&
+          (n as Element).tagName === 'BODY'
+        ) {
+          const script = doc.createElement('script');
+          script.setAttribute('src', CLIENT_CODE_REQ);
+          (n as Element).appendChild(script);
+          break;
+        }
+      }
+    }
+
+    const html = doc.toString();
+    res.header('Content-Type', 'text/html;charset=UTF-8');
+    res.send('<!doctype html>\n' + html);
+  }
+}
+
+function serveErrorPage(errors: PageError[], res: Response) {
+  const p = new Array<string>();
+  p.push(`<!doctype html><html><head>
+    <title>Page Error</title>
+    <meta name="color-scheme" content="light dark"/>
+    </head><body><ul>`);
+  errors.forEach(err => {
+    const l = err.loc;
+    p.push(`<li>${err.msg}`);
+    l && p.push(` - ${l.source} `);
+    l && p.push(`[${l.start.line}, ${l.start.column + 1}]`);
+    p.push('</li>');
+  });
+  p.push('</ul></body></html>');
+  res.header('Content-Type', 'text/html;charset=UTF-8');
+  // res.sendStatus(500);
+  res.send(p.join(''));
+}
---
+++ test/server/server.test.ts
@@ -0,0 +1,83 @@
+import fs from 'fs';
+import { Browser, BrowserPage } from 'happy-dom';
+import path from 'path';
+import { afterAll, afterEach, assert, beforeAll, it } from 'vitest';
+import { Server } from '../../src/server/server';
+import { normalizeText } from '../../src/html/parser';
+import { CLIENT_CODE_REQ } from '../../src/server/middleware';
+
+const docroot = path.join(__dirname, 'server');
+const server = new Server({
+  docroot,
+  mute: true,
+  clientCodePath: path.join(__dirname, '../../dist/client.js'),
+});
+const files = (() => {
+  const ret = new Array<string>();
+  const lookupIn = (dir: string) => {
+    fs.readdirSync(path.join(docroot, dir)).forEach(file => {
+      const relPath = path.join(dir, file);
+      const absPath = path.join(docroot, relPath);
+      const stat = fs.statSync(absPath);
+      if (stat.isFile() && file.endsWith('-in.html')) {
+        ret.push(relPath);
+      } else if (stat.isDirectory()) {
+        lookupIn(relPath);
+      }
+    });
+  }
+  lookupIn('.');
+  return ret.sort();
+})();
+let browser: Browser | null = null;
+
+beforeAll(async () => {
+  await server.start();
+});
+
+afterAll(async () => {
+  await server.stop();
+});
+
+afterEach(async () => {
+  await browser?.close();
+  browser = null;
+});
+
+it('should deliver client code', async () => {
+  const res = await fetch(`http://127.0.0.1:${server.port}${CLIENT_CODE_REQ}`);
+  assert.isTrue(res.ok);
+  assert.isNotEmpty(await res.text());
+});
+
+files.forEach(file => {
+
+  it(file, async () => {
+    browser = new Browser({ settings: { disableJavaScriptFileLoading: true } });
+    const page = browser.newPage();
+    await page.goto(`http://127.0.0.1:${server.port}/${file}`);
+    const actual = getActual(page);
+    const expected = getExpected(file, '-out.html');
+    assert.equal(
+      normalizeText(actual),
+      normalizeText(expected)
+    );
+  });
+
+});
+
+function getActual(page: BrowserPage) {
+  let ret = page.mainFrame.document.documentElement.outerHTML;
+  // ret = ret.replace('<html><head>', '<html>\n<head>');
+  // ret = ret.replace('</body></html>', '</body>\n</html>');
+  ret = ret.trim();
+  return ret;
+}
+
+function getExpected(file: string, suffix: string) {
+  file = file.replace(/-in\.html$/, suffix);
+  const pathname = path.join(docroot, file);
+  let ret = fs.readFileSync(pathname).toString();
+  ret = ret.trim();
+  return ret;
+}
